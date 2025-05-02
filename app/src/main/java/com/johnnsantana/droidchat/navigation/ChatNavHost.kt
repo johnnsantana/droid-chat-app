985d67244a50ca5e3a07a55c9f2803a7c26ce7c6
Jhônatan Santana <jhonatansantanati@gmail.com>
2025-05-02 10:56:37 -0300

    style: style of text noAccountRegister

--- app/src/main/java/com/johnnsantana/droidchat/navigation/ChatNavHost.kt
+++ app/src/main/java/com/johnnsantana/droidchat/navigation/ChatNavHost.kt
@@ -33,7 +33,11 @@ fun ChatNavHost() {
             )
         }
         composable<SignInRoute> {
-            SignInRoute()
+            SignInRoute(
+                navigateToSignUp = {
+                    navController.navigate(SignUpRoute)
+                }
+            )
         }
         composable<SignUpRoute> {
 
--- app/src/main/java/com/johnnsantana/droidchat/ui/feature/sigin/SignInScreen.kt
+++ app/src/main/java/com/johnnsantana/droidchat/ui/feature/sigin/SignInScreen.kt
@@ -4,20 +4,31 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -29,19 +40,22 @@ import com.johnnsantana.droidchat.ui.theme.DroidChatTheme
 
 @Composable
 fun SignInRoute(
-    viewModel: SigInViewModel = viewModel()
+    viewModel: SigInViewModel = viewModel(),
+    navigateToSignUp: () -> Unit
 ) {
     var formState = viewModel.formState
     SigInScreen(
         formState = formState,
-        onFormEvent = viewModel::onEvent
+        onFormEvent = viewModel::onEvent,
+        onRegisterClick = navigateToSignUp
     )
 }
 
 @Composable
 fun SigInScreen(
     formState: SigInFormState,
-    onFormEvent: (SignInFormEvent) -> Unit = {}
+    onFormEvent: (SignInFormEvent) -> Unit = {},
+    onRegisterClick: () -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -104,6 +118,41 @@ fun SigInScreen(
 
         )
 
+        Spacer(Modifier.height(56.dp))
+
+        val noAccountLabel = stringResource(id = R.string.feature_login_no_account)
+        val registerLabel = stringResource(id = R.string.feature_login_register)
+        val annotatedString = buildAnnotatedString {
+            val text = "$noAccountLabel $registerLabel"
+            val startIndex = text.indexOf(registerLabel)
+            val endIndex = startIndex + registerLabel.lastIndex + 1
+
+            append(text)
+            addStyle(
+                style = SpanStyle(
+                    color = Color.White,
+                ),
+                start = 0,
+                end = startIndex
+            )
+            addStyle(
+                style = SpanStyle(
+                    color = MaterialTheme.colorScheme.primary,
+                    textDecoration = TextDecoration.Underline
+                ),
+                start = startIndex,
+                end = endIndex
+            )
+
+            addStringAnnotation(
+                tag = "route",
+                annotation = "register",
+                start = startIndex,
+                end = endIndex
+            )
+        }
+
+        Text(text = annotatedString)
     }
 }
 
@@ -112,7 +161,8 @@ fun SigInScreen(
 private fun SignInScreenPreview() {
     DroidChatTheme {
         SigInScreen(
-            formState = SigInFormState()
+            formState = SigInFormState(),
+            onRegisterClick = {}
         )
     }
 }
