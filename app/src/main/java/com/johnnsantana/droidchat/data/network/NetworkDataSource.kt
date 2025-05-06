7ec45c487af2cd46bd83da35497c673c6d956648
Jhônatan Santana <jhonatansantanati@gmail.com>
2025-05-06 01:31:52 -0300

    feat: implements datasource

---
+++ app/src/main/java/com/johnnsantana/droidchat/data/network/NetworkDataSource.kt
@@ -0,0 +1,10 @@
+package com.johnnsantana.droidchat.data.network
+
+import com.johnnsantana.droidchat.data.network.model.AuthRequest
+import com.johnnsantana.droidchat.data.network.model.CreateAccountRequest
+import com.johnnsantana.droidchat.data.network.model.TokenResponse
+
+interface NetworkDataSource {
+    suspend fun signUp(request: CreateAccountRequest)
+    suspend fun signIn(request: AuthRequest) : TokenResponse
+}
---
+++ app/src/main/java/com/johnnsantana/droidchat/data/network/NetworkDataSourceImpl.kt
@@ -0,0 +1,28 @@
+package com.johnnsantana.droidchat.data.network
+
+import com.johnnsantana.droidchat.data.network.model.AuthRequest
+import com.johnnsantana.droidchat.data.network.model.CreateAccountRequest
+import com.johnnsantana.droidchat.data.network.model.TokenResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import javax.inject.Inject
+
+class NetworkDataSourceImpl @Inject constructor(
+    private val httpClient: HttpClient
+) : NetworkDataSource {
+
+    override suspend fun signUp(request: CreateAccountRequest) {
+        httpClient.post("signup") {
+            setBody(request)
+        }.body<Unit>()
+    }
+
+    override suspend fun signIn(request: AuthRequest): TokenResponse {
+        return httpClient.post("signin") {
+            setBody(request)
+        }.body<TokenResponse>()
+    }
+
+}
